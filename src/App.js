ef3bd6aedea25233b41c3a562cab47907bc288a9
Ever <ever.aliaga@gmail.com>
2019-02-28 00:16:10 -0300

    Movies

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
+import 'bulma/css/bulma.css';
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { MovieList } from './components/MoviesList';
+import { SearchForm } from './components/SearchForm';
+import { Title } from './components/Title';
+
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        usedSearch: false,
+        results: []
+    }
+
+    _handleResults = (results) => {
+        this.setState({ results: results, usedSearch: true });
+    }
+
+    _renderResults = () => {
+        return this.state.results.length === 0
+                ? <p>Sorry!, results not found</p>
+                : <MovieList movies={this.state.results} />
+    }
+
+    render() {
+        return (
+        <div className="App">
+            <Title>Movies</Title> 
+            <div className="SearchForm-wrapper">
+                <SearchForm onResults={this._handleResults} />
+            </div>
+            {
+                this.state.usedSearch 
+                ? this._renderResults()
+                : <small>Use the formto search a movie</small>
+            }
+            
+        </div>
+        );
+    }
 }
 
 export default App;
